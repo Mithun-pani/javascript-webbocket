961d3385c863231b8dec73f0a6db4c6054d366e4
Mithun-pani <152254839+Mithun-pani@users.noreply.github.com>
2023-11-28 07:27:48 +0000

    javascriptbasic dunk

---
+++ basic/test.js
@@ -0,0 +1,7 @@
+console.log("hello world")
+/* */javascript is a high level scripting language used to perform the task on frontend as well as backend.
+/* */javascript use both clientside as well as serverside.
+/* */javascript is most used programming language for web devlopement.
+/* */in javascript we use "esb" version is the latest one .devloped in 2015.
+*esb=echma script6(vanilla javascript)
+/* */javascript
